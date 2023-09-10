1c90979cdc9ba2cfa0f25d4f0117cf37e34f5799
natalia_znamierowska <nat.znamierowska@gmail..com>
2023-09-11 00:56:23 +0200

    fix app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import "./App.css";
-import "./styles/About"
-import "./styles/Navbar"
-import "./styles/Offer"
-import "./styles/Footer"
-import "./styles/Header"
+import "./styles/About.css"
+import "./styles/Navbar.css"
+import "./styles/Offer.css"
+import "./styles/Footer.css"
+import "./styles/Header.css"
 import Navbar from "./components/Navbar/Navbar";
 import Header from "./components/Header/Header";
 import About from "./components/About/About";
